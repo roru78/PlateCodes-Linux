5a0fddd40d0207a8f5e82685fcae5abc0e2f9e25
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-05-02 19:22:42 +0800

    Stopped tracking build
    Start tracking .gitignore

--- build/CMakeFiles/platecodes.desktop.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/platecodes.desktop"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/platecodes.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
